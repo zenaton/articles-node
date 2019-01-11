e6879bfcdbcfc3ca5168fcace0762d0cd2848da1
François Guillot <francois@zenaton.com>
2019-01-11 19:13:15 +0100

    Add some randomness in the custom id

--- rent-payment-workflow/index.js
+++ rent-payment-workflow/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const uuid = require("uuid/v4");
 
 const CollectRentWorkflow = require("./workflows/collect-rent-workflow");
 
@@ -7,8 +8,8 @@ const CollectRentWorkflow = require("./workflows/collect-rent-workflow");
 require("./client");
 
 // Start the workflow
-const FAKE_PLACE_ID = "FOO";
-const FAKE_TENANT_ID = "BAR";
+const FAKE_PLACE_ID = uuid();
+const FAKE_TENANT_ID = uuid();
 const MONTH = "0";
 const YEAR = "2019";
 
@@ -17,30 +18,36 @@ new CollectRentWorkflow({
   tenantId: FAKE_TENANT_ID,
   month: MONTH,
   year: YEAR
-}).dispatch().catch((err) => {
-  console.error(err);
-});
+})
+  .dispatch()
+  .catch(err => {
+    console.error(err);
+  });
+
+console.log("Use this curl to trigger rent payment");
+console.log(
+  `curl -X POST 'http://localhost:8080/pay-rent/${FAKE_PLACE_ID}/${FAKE_TENANT_ID}?month=${MONTH}&year=${YEAR}'`
+);
 
 // Start the Express app
 const app = express();
 
-app.post("/pay-rent/:placeId/:tenantId", function (req, res, next) {
+app.post("/pay-rent/:placeId/:tenantId", function(req, res, next) {
   const { placeId, tenantId } = req.params;
   const { month, year } = req.query;
 
   const workflowInstanceId = `TENANT#${tenantId}-PLACE#${placeId}-MONTH#${month}-YEAR#${year}`;
 
-  CollectRentWorkflow
-    .whereId(workflowInstanceId)
+  CollectRentWorkflow.whereId(workflowInstanceId)
     .send("RentPaid", {})
-    .then(() => {
-      res.sendStatus(200);
-    }, (err) => {
-      next(err);
-    });
+    .then(
+      () => {
+        res.sendStatus(200);
+      },
+      err => {
+        next(err);
+      }
+    );
 });
-
-// Use this curl to trigger rent payment
-// curl -X POST 'http://localhost:8080/pay-rent/FOO/BAR?month=0&year=2019'
 
 app.listen(8080);
